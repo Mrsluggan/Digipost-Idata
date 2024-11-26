9e0ddf5b7174cb0225a5faa7a6f6c0366e11b340
mrsluggan <ericflyger@gmail.com>
2024-11-26 11:01:42 +0100

    first comit

---
+++ src/main/java/com/idata/digipost/DigipostApplication.java
@@ -0,0 +1,13 @@
+package com.idata.digipost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigipostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigipostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idata/digipost/DigipostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idata.digipost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigipostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
