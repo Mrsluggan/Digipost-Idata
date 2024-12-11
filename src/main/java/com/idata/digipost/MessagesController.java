c6f84b75b25a43aace9b711c1b9299ca861ca925
mrsluggan <ericflyger@gmail.com>
2024-12-11 11:38:46 +0100

    Update MessagesController.java

--- src/main/java/com/idata/digipost/MessagesController.java
+++ src/main/java/com/idata/digipost/MessagesController.java
@@ -1,6 +1,7 @@
 package com.idata.digipost;
 
 import no.digipost.api.client.representations.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Logger;
 
 
 @RestController
@@ -15,6 +17,8 @@ import java.util.List;
 public class MessagesController {
 
    MessageService messageService;
+    private final Logger logger = Logger.getLogger(MessagesController.class.getName()); // Initialize logger
+
 
    public MessagesController(MessageService messageService){
        this.messageService = messageService;
@@ -23,11 +27,9 @@ public class MessagesController {
     @PostMapping()
     public ResponseEntity<MessageDelivery> sendMessage(@RequestPart List<MultipartFile> document, String subject,String recipient) throws IOException {
         for (MultipartFile file : document) {
-            System.out.println(file.getOriginalFilename());
+            logger.info("These are the files: "+file.getOriginalFilename());
         }
-
-
-       System.out.println(subject);
+        logger.info("Starting to send message");
         return ResponseEntity.ok(messageService.sendMessage(recipient,subject,document));
     }
 
