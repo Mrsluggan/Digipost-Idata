8fb594eff496c2921961994946003d88e1a94c8a
csro23ei <dante030724@gmail.com>
2024-12-12 23:49:16 +0100

    Archive

---
+++ src/main/java/com/idata/digipost/Archive/Archive.java
@@ -0,0 +1,46 @@
+package com.idata.digipost.Archive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Archive {
+
+    private final List<ArchiveDocument> documents;
+    private final long senderId;
+
+    private Archive(Builder builder) {
+        this.documents = builder.documents;
+        this.senderId = builder.senderId;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public List<ArchiveDocument> getDocuments() {
+        return documents;
+    }
+
+    public long getSenderId() {
+        return senderId;
+    }
+
+    public static class Builder {
+        private List<ArchiveDocument> documents = new ArrayList<>();
+        private long senderId;
+
+        public Builder documents(List<ArchiveDocument> documents) {
+            this.documents = documents;
+            return this;
+        }
+
+        public Builder senderId(long senderId) {
+            this.senderId = senderId;
+            return this;
+        }
+
+        public Archive build() {
+            return new Archive(this);
+        }
+    }
+}
