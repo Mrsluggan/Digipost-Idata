0e68def0a037f20a6c2d833117af92a89e78df1f
csro23ei <dante030724@gmail.com>
2024-12-16 23:44:17 +0100

    message test update och Archive lagd på is

--- src/main/java/com/idata/digipost/Models/InvoiceDTO.java
+++ src/main/java/com/idata/digipost/Models/InvoiceDTO.java
@@ -8,17 +8,21 @@ import no.digipost.api.datatypes.types.ExternalLink;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class InvoiceDTO {
 
+    public InvoiceDTO(String string, String string2, double d, String creditorAccount2, String kid2) {
+        // TODO Auto-generated constructor stub
+    }
+
     private ExternalLink link;
     private ZonedDateTime dueDate;
     private BigDecimal sum;
     private String creditorAccount;
     private String kid;
-
 
 }
