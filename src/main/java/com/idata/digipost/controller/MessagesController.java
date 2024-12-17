886aa803e9b3661245450104f05fd251af1dd465
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-12-17 10:42:32 +0100

    OOP

--- src/main/java/com/idata/digipost/controller/MessagesController.java
+++ src/main/java/com/idata/digipost/controller/MessagesController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.idata.digipost.models.Request;
+
 import java.util.List;
 import java.util.logging.Logger;
 
